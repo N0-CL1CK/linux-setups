65c374b00aa3b6efd1d04671b3e11c67db1beb5d
Luís Gonzaga <53665362+N0-CL1CK@users.noreply.github.com>
2023-07-20 16:47:33 -0400

    Update setup-ubuntu.sh

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -14,17 +14,15 @@ yes | {
 
 
   ## Development Softwares
-  sudo apt install git sassc libglib2.0-dev-bin libglib2.0-dev libxml2-utils chrome-gnome-shell gnome-tweaks fonts-jetbrains-mono jq
+  sudo apt install git fonts-jetbrains-mono jq
   sudo snap install code --classic
   sudo snap install insomnia
-  sudo snap install node --classic
 
 
   ## Node set up
-  mkdir /home/$USER/.npm-global
-  npm config set prefix '~/.npm-global'
-  ## Precisa reinstalar o npm globalmente ou movê-lo para ~/.npm-global
-  npm install -g npm@9.6.6
+  wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+  source ~/.zshrc
+  nvm install lts/hydrogen
 
   ## Git set up
   if [ -f "./config.json" ]; then
@@ -40,6 +38,7 @@ yes | {
   fi
 
   ## Themes
+  sudo apt install sassc libglib2.0-dev-bin libglib2.0-dev libxml2-utils chrome-gnome-shell gnome-tweaks
   git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git /home/$USER/Downloads/WhiteSur
   chmod +x /home/$USER/Downloads/WhiteSur/install.sh
   chmod +x /home/$USER/Downloads/WhiteSur/tweaks.sh
